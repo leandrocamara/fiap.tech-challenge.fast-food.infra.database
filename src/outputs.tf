25f5e1e739f175fee0a07a33fc92ba283a41b10e
mpatricio007 <mpatricio007@gmail.com>
2024-07-14 18:37:09 -0300

    add-dynamodb

--- src/outputs.tf
+++ src/outputs.tf
@@ -5,7 +5,3 @@ output "ordersDbEndpoint" {
 output "paymentsDbEndpoint" {
   value = aws_db_instance.db_payments_instance.endpoint
 }
-
-output "ticketsDbEndpoint" {
-  value = aws_dynamodb_table.tickes_table.endpoint
-}
